28671cc7eaa05894e28f98e10d00941ab30b0e56
Robin-Kurian <dev.robinkurian@gmail.com>
2025-01-07 16:44:52 +0530

    Updated personal information in data.json, including name, contact details, skills, technical expertise, experience, projects, education, interests, languages, and strengths. Enhanced index.html with new sections for education and strengths, and added GitHub links. Modified scriptUsingJson.js to reflect changes in data structure and improve project details display. Updated style.css for better layout and styling adjustments. Replaced Profile.png with a new version.

--- scriptUsingJson.js
+++ scriptUsingJson.js
@@ -11,8 +11,10 @@ fetch('./data.json')
         document.getElementById("titleName").innerHTML = `Resume | ${personalInfo.firstName}`;
         document.getElementById("firstName").innerHTML = personalInfo.firstName;
         document.getElementById("lastName").innerHTML = personalInfo.lastName;
-        document.querySelector(".contactDetails .fa-envelope").parentElement.innerHTML = `<a href="mailto:${personalInfo.email}"><i class="fa fa-envelope">&nbsp;&nbsp;</i>${personalInfo.email}</a>`;
-        document.querySelector(".contactDetails .fa-phone").parentElement.innerHTML = `<a href="tel:+91${personalInfo.phone}"><i class="fa fa-phone" aria-hidden="true">&nbsp;&nbsp;</i>${personalInfo.phone}</a>`;
+        document.getElementById("team-github").parentElement.innerHTML = `<a href="${personalInfo.teamGithub}" target="_blank"><i class="fa fa-github" aria-hidden="true">&nbsp;&nbsp;</i>Team GitHub</a>`;
+        document.getElementById("github").parentElement.innerHTML = `<a href="${personalInfo.github}" target="_blank"><i class="fa fa-github" aria-hidden="true">&nbsp;&nbsp;</i>Personal</a>`;
+        document.querySelector(".contactDetails .fa-envelope").parentElement.innerHTML = `<a href="mailto:${personalInfo.email}" ><i class="fa fa-envelope">&nbsp;&nbsp;</i>${personalInfo.email}</a>`;
+        document.querySelector(".contactDetails .fa-phone").parentElement.innerHTML = `<a href="tel:+91${personalInfo.phone}"><i class="fa fa-phone" aria-hidden="true">&nbsp;&nbsp;</i>+91 ${personalInfo.phone}</a>`;
         document.getElementById("avatar").src = personalInfo.profileImage;
 
         // Populate objective
@@ -38,7 +40,7 @@ fetch('./data.json')
         const experienceList = document.getElementById("experienceList");
         personalInfo.experience.forEach((exp, index) => {
             const li = document.createElement("li");
-            li.innerHTML = `<span class="subDetails">${exp.title} &nbsp;</span><span class="bold"></span>- ${exp.company}, ${exp.location} <br>${exp.duration}${index < personalInfo.experience.length - 1 ? '<br><br>' : ''}`;
+            li.innerHTML = `<span class="subDetails">${exp.title} &nbsp;</span><span class="bold"></span>- ${exp.company}, ${exp.location} <br> ${exp.duration} <br>${exp.highlights}${index < personalInfo.experience.length - 1 ? '<br><br>' : ''}`;
             experienceList.appendChild(li);
         });
 
@@ -49,7 +51,7 @@ fetch('./data.json')
             const li = document.createElement("li");
             let projectLink = '';
             if (project.link) {
-                projectLink += `<a href="${project.link}" target="_blank">View Project</a> &nbsp;&nbsp;`;
+                projectLink += `<a href="${project.link}" target="_blank">Live Demo</a> &nbsp;&nbsp;`;
             }
             if (project.github) {
                 projectLink += `<a href="${project.github}" target="_blank">GitHub</a>`;
@@ -57,7 +59,7 @@ fetch('./data.json')
             if (project.github2) {
                 projectLink += `<a href="${project.github2}" target="_blank">&nbsp;&nbsp; GitHub(Backend)</a>`;
             }
-            let projectDetails = `<span class="subDetails">${project.title} &nbsp;</span><span class="bold"></span>- ${project.description} <br>`;
+            let projectDetails = `<span class="subDetails">${project.title} &nbsp;</span><span class="bold"></span> [<span class="projectTech">${project.technologies.join(', ')}</span>] <br> <span class="projectHighlights">${project.description}</span> <br>`;
             if (project.duration) {
                 projectDetails += `${project.duration} <br>`;
             }
--- scriptWithData.js
+++ scriptWithData.js
@@ -1,97 +1,3 @@
-// resume.js
-// Define an object to hold personal information
-const personalInfo = {
-    firstName: "John",
-    lastName: "Doe",
-    email: "john.doe@example.com",
-    phone: "1234567890",
-    profileImage: "Profile.png",
-    objective: "To obtain a software developer role where I can leverage my technical expertise to develop creative solutions that enhance user experience and drive organizational growth, while also advancing my professional development.",
-    "skills": [
-        "Basic Programming",
-        "Web Development",
-        "Team Collaboration",
-        "Problem Solving",
-        "Communication",
-        "Learning Agility"
-    ],
-    "technicalExpertise": [
-        "HTML",
-        "CSS",
-        "JavaScript",
-        "Python",
-        "Git",
-        "SQL"
-    ],
-    "experience": [
-        {
-            "title": "Junior Software Developer",
-            "company": "Tech Innovations",
-            "location": "Cityville",
-            "duration": "(Jan 2023 - Present)"
-        },
-        {
-            "title": "Intern Software Developer",
-            "company": "Code Academy",
-            "location": "Townsville",
-            "duration": "(Jun 2022 - Dec 2022)"
-        }
-    ],
-    "projects": [
-        {
-            "title": "Personal Portfolio Website",
-            "description": "Created a personal portfolio website to showcase projects and skills using HTML, CSS, and JavaScript.",
-            "technologies": [
-                "HTML",
-                "CSS",
-                "JavaScript"
-            ],
-            "link": "https://john-doe-portfolio.com",
-            "github": "https://github.com/johndoe/portfolio-website",
-            "github2": "https://github.com/johndoe/portfolio-website-backend",
-            "duration": "(Nov 2022 - Dec 2022)"
-        },
-        {
-            "title": "Simple To-Do List App",
-            "description": "Developed a simple to-do list application using JavaScript and local storage.",
-            "technologies": [
-                "JavaScript",
-                "HTML",
-                "CSS"
-            ],
-            "link": "https://john-doe-todo-app.com",
-            "github": "https://github.com/johndoe/todo-app",
-            "duration": "(Jan 2023 - Feb 2023)"
-        }
-    ],
-    "education": [
-        {
-            "degree": "BSc in Computer Science",
-            "institution": "City University",
-            "duration": "(Aug 2019 - May 2022)"
-        },
-        {
-            "degree": "High School Diploma",
-            "institution": "Townsville High School",
-            "duration": "(Jun 2015 - May 2019)"
-        }
-    ],
-    "interests": [
-        "Coding",
-        "Gaming",
-        "Reading",
-        "Traveling"
-    ],
-    "languages": [
-        "English",
-        "Spanish"
-    ],
-    "strengths": [
-        "Eager Learner",
-        "Adaptable",
-        "Team Player"
-    ]
-};
 
 // Insert data into the HTML
 document.addEventListener("DOMContentLoaded", function () {
@@ -126,9 +32,9 @@ document.addEventListener("DOMContentLoaded", function () {
     const experienceList = document.getElementById("experienceList");
     personalInfo.experience.forEach((exp, index) => {
         const li = document.createElement("li");
-            li.innerHTML = `<span class="subDetails">${exp.title} &nbsp;</span><span class="bold"></span>- ${exp.company}, ${exp.location} <br>${exp.duration}${index < personalInfo.experience.length - 1 ? '<br><br>' : ''}`;
-            experienceList.appendChild(li);
-        });
+        li.innerHTML = `<span class="subDetails">${exp.title} &nbsp;&nbsp;</span><span class="bold"></span>- ${exp.company}, ${exp.location} <br>${exp.duration}${index < personalInfo.experience.length - 1 ? '<br><br>' : ''}`;
+        experienceList.appendChild(li);
+    });
 
 
     // Populate projects section
@@ -140,27 +46,24 @@ document.addEventListener("DOMContentLoaded", function () {
             projectLink += `<a href="${project.link}" target="_blank">View Project</a> &nbsp;&nbsp;`;
         }
         if (project.github) {
-                projectLink += `<a href="${project.github}" target="_blank">GitHub</a>`;
-            }
-            if (project.github2) {
-                projectLink += `<a href="${project.github2}" target="_blank">&nbsp;&nbsp; GitHub(Backend)</a>`;
-            }
-            let projectDetails = `<span class="subDetails">${project.title} &nbsp;</span><span class="bold"></span>- ${project.description} <br>`;
-            if (project.duration) {
-                projectDetails += `${project.duration} <br>`;
-            }
-            projectDetails += `${projectLink}${index < personalInfo.projects.length - 1 ? '<br><br>' : ''}`;
-            li.innerHTML = projectDetails;
-            projectsList.appendChild(li);
-        });
+            projectLink += `<a href="${project.github}" target="_blank">Check GitHub</a>`;
+        }
+        let projectDetails = `<span class="subDetails">${project.title} &nbsp;&nbsp;</span><span class="bold"></span> - ${project.description} <br>`;
+        if (project.duration) {
+            projectDetails += `${project.duration} <br>`;
+        }
+        projectDetails += `${projectLink}${index < personalInfo.projects.length - 1 ? '<br><br>' : ''}`;
+        li.innerHTML = projectDetails;
+        projectsList.appendChild(li);
+    });
 
     // Populate education section
     const educationList = document.getElementById("educationList");
     personalInfo.education.forEach((edu, index) => {
         const li = document.createElement("li");
-            li.innerHTML = `<span class="subDetails">${edu.degree} &nbsp;</span><span class="bold"></span>- ${edu.institution} <br>${edu.duration}${index < personalInfo.education.length - 1 ? '<br><br>' : ''}`;
-            educationList.appendChild(li);
-        });
+        li.innerHTML = `<span class="subDetails">${edu.degree} &nbsp;&nbsp;</span><span class="bold"></span> - ${edu.institution} <br>${edu.duration}${index < personalInfo.education.length - 1 ? '<br><br>' : ''}`;
+        educationList.appendChild(li);
+    });
 
     // Populate interests section
     const interestsList = document.getElementById("interestsList");
